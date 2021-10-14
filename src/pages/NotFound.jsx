e42984c005668b2cd999c299bfb3273d3453f370
mia-moran <mariamoranluaces@gmail.com>
2021-10-14 14:26:55 +0200

    2 pages + 2 containers + route system added

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../styles/global.css';
+import '../styles/NotFound.scss';
+const NotFound = () => {
+    return(
+        <div class="not-found-container">
+            <div class="not-found">
+                <h1>404</h1>
+                <h2>Error, Not found</h2>
+            </div>
+        </div>
+    );
+}
+
+export default NotFound;
